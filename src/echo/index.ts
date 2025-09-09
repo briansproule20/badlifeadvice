0859fdff99088f18bdc65f77a07d0db9e491e187
Brian Sproule <briansproule20@gmail.com>
2025-09-09 13:58:05 -0400

    chore: push latest echo changes (no env files)

--- src/echo/index.ts
+++ src/echo/index.ts
@@ -1,4 +1,4 @@
 import Echo from "@merit-systems/echo-next-sdk";
 export const { handlers, isSignedIn, openai, anthropic } = Echo({
-    appId: "e595f23c-18b6-4cd2-a44f-60591bcea32f",
+    appId: process.env.NEXT_PUBLIC_ECHO_APP_ID!,
 });
